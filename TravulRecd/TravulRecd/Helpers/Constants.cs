900ac1dc672c729b70403d3d4a3391836a4aff54
isaac ikusika <isaacikusika@gmail.com>
2020-08-30 17:34:51 +0100

    updated privacy

--- TravulRecd/TravulRecd/Helpers/Constants.cs
+++ TravulRecd/TravulRecd/Helpers/Constants.cs
@@ -7,8 +7,8 @@ namespace TravulRecd.Helpers
     public class Constants
     {
         public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
-        public const string CLIENT_SECRET = "PM2RN5YL3SWA1ZSB2WRPGWBKIT2WBTBCKS0QC4UWLCLXCRRZ";
-        public const string CLIENT_ID = "JDUE4N5OGSDH0YENBVRKJ1XHOKVPCUGODNJ0OZHC53WMRNNB";
+        public const string CLIENT_SECRET = "new client secret";
+        public const string CLIENT_ID = "new client id";
 
     }
 }
